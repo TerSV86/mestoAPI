7d567835e5a8ef0c6f8dfff81a925c51cfd842b6
Sergey Terpugov <TerSV@yandex.ru>
2024-05-03 16:09:49 +0500

    обработка ошибок вынесена в отдельную функцию

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import { errors } from 'celebrate';
 import { requestLogger, errorLogger } from './middleware/logger';
 import router from './routes/index';
 import { CustomRequest } from './types/customTypes';
+import errorHandler from './middleware/errorHandler';
 
 require('dotenv').config();
 
@@ -31,36 +32,7 @@ mongoose.connection.on('error', (err) => {
 
 app.use(errorLogger);
 app.use(errors());
-app.use((err: any, req: CustomRequest, res: Response, next: NextFunction) => {
-  console.log(err.name);
-  if (err.name === 'JsonWebTokenError') {
-    return res.status(constants.HTTP_STATUS_UNAUTHORIZED)
-      .send({ message: 'Токен не действителен' });
-  }
-
-  if (err instanceof Error && err.message.startsWith('E11000')) {
-    return res.status(constants.HTTP_STATUS_CONFLICT)
-      .send({ message: 'Пользователь с таким email уже существует' });
-  }
-
-  if (err.name === 'ValidationError') {
-    return res.status(constants.HTTP_STATUS_BAD_REQUEST)
-      .send({ message: `Переданы некорректные данные ${err.message}` });
-  }
-  if (err instanceof MongooseError.CastError) {
-    return res.status(constants.HTTP_STATUS_BAD_REQUEST)
-      .send({ message: 'Передан некорректный id' });
-  }
-  const { statusCode = 500, message } = err;
-
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере ошибка'
-        : message,
-    });
-});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Приложение работает на порту ${PORT}`);
---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+import { constants } from 'http2';
+import mongoose, { Error as MongooseError } from 'mongoose';
+import { CustomRequest } from '../types/customTypes';
+
+const errorHandler = (err: any, req: CustomRequest, res: Response, next: NextFunction) => {
+  if (err.name === 'JsonWebTokenError') {
+    return res.status(constants.HTTP_STATUS_UNAUTHORIZED)
+      .send({ message: 'Токен не действителен' });
+  }
+
+  if (err instanceof Error && err.message.startsWith('E11000')) {
+    return res.status(constants.HTTP_STATUS_CONFLICT)
+      .send({ message: 'Пользователь с таким email уже существует' });
+  }
+
+  if (err.name === 'ValidationError') {
+    return res.status(constants.HTTP_STATUS_BAD_REQUEST)
+      .send({ message: `Переданы некорректные данные ${err.message}` });
+  }
+  if (err instanceof MongooseError.CastError) {
+    return res.status(constants.HTTP_STATUS_BAD_REQUEST)
+      .send({ message: 'Передан некорректный id' });
+  }
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере ошибка'
+        : message,
+    });
+};
+
+export default errorHandler;
